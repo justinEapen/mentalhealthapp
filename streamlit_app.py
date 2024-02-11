cda7f4b1f31054ab7e566d2b5d7377c81bdecbb9
Justin Eapen George <70130562+justinEapen@users.noreply.github.com>
2024-02-11 21:42:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,68 +2,46 @@ import pip
 
 pip.main(['install', 'cohere'])
 
-import cohere
 import streamlit as st
+from home import page
+from test import quiz
+from anxiety import anx
+from depression import dep
+from chatbot import chat
 
-co = cohere.Client('18V1Oo06GAf0xMaXbBjkHlhdHktqbjc5tusZHZMV') # This is your trial API key
-
-st.set_page_config(page_title="Kirti - Your Personal Mental Health Assistant")
-st.title("Mental Health Bot")
-
-preamble_prompt = """You are an AI Mental Health Therapist named "Kirti". You are a mental health therapist chatbot designed to provide empathetic and supportive responses to individuals seeking help with their emotional well-being. Your goal is to create a safe and non-judgmental space for users to express their feelings, thoughts, and concerns. Actively listen, offer thoughtful insights, and provide guidance that encourages self-reflection and positive coping strategies. Keep in mind the importance of maintaining user privacy and confidentiality. If the user expresses thoughts of self-harm or harm to others, prioritize safety by encouraging them to seek professional help or contacting emergency services. Remember to approach each interaction with empathy and respect, fostering a therapeutic environment through your responses. 
-Gather all the necessary information such as name, gender, age category, and any extras they may want to add.
-Ask these questions one after another. DO NOT ASK EVERYTHING AT ONCE. Get the information one at a time.
-If you don't know the answer to any query, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context."""
-
-
-docs = [
-    {
-        "name" : "Mental Health"
+st.set_page_config(
+    page_title="We Got You",
+    page_icon="Images/quiz3.png",
+    menu_items={
+        'About':"Developed by - https://github.com/vimalgupta24"
     }
-]
+)
 
-def cohereChat(prompt):
-    llm_response = co.chat(
-        model='command',
-        message=prompt,
-        preamble_override = preamble_prompt,
-        documents=docs,
-    )
+st.markdown("""
+	<style>
+	/* This is to hide hamburger menu completely */
+	/* #MainMenu {visibility: hidden;} */
+	/* This is to hide Streamlit footer */
+	footer {visibility: hidden;}
+	ul[data-testid=main-menu-list] > li:nth-of-type(4), /* Documentation */
+	ul[data-testid=main-menu-list] > li:nth-of-type(5), /* Ask a question */
+	ul[data-testid=main-menu-list] > li:nth-of-type(6), /* Report a bug */
+	ul[data-testid=main-menu-list] > li:nth-of-type(7), /* Streamlit for Teams */
+	ul[data-testid=main-menu-list] > div:nth-of-type(2) /* 2nd divider */
+		{display: none; visibility:hidden;}
+	</style>
+""", unsafe_allow_html=True)
 
-    return llm_response
-
-def initialize_state():
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-def main():
-
-    initialize_state()
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
-    # React to user input
-    if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        llm_response = cohereChat(prompt)
-
-        response = f"{llm_response.text}"
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-    
-
-if __name__ == "__main__" :
-    main()
+st.sidebar.title("What to do :)")
+app=st.sidebar.selectbox("Choose the app mode",
+        ["Home", "Depression", "Anxiety","Quiz"])
+if app=="Home":
+    page()
+elif app=="Depression":
+    dep()
+elif app=="Anxiety":
+    anx()
+elif app=="Quiz":
+    quiz()
+elif app=="Chatbot":
+    chat()
