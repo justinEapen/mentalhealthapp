892e1b1117614594d273ffccb1aa71b698db67c8
Justin Eapen George <70130562+justinEapen@users.noreply.github.com>
2024-05-02 12:11:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import pip
 import cohere
 import streamlit as st
 
@@ -22,47 +21,65 @@ docs = [
     }
 ]
 
-def cohereChat(prompt):
-    llm_response = co.chat(
-        model='command-r-plus',
-        message=prompt,
-        preamble = preamble_prompt,
-        documents=docs,
-    )
 
-    return llm_response
+def cohereReply(prompt):
 
-def initialize_state():
+    # Extract unique roles using a set
+    unique_roles = set(item['role'] for item in st.session_state.messages)
+
+    if {'USER', 'assistant'} <= unique_roles:
+        # st.write("INITIAL_________________")
+        llm_response = co.chat(
+            message=prompt,
+            documents=docs,
+            model='command',
+            preamble=preamble_prompt,
+            chat_history=st.session_state.messages,
+        )
+    else:
+
+        llm_response = co.chat(
+            message=prompt,
+            documents=docs,
+            model='command',
+            preamble=preamble_prompt,
+
+        )
+
+    print(llm_response)
+    return llm_response.text
+
+
+def initiailize_state():
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
+
 def main():
 
-    initialize_state()
+    initiailize_state()
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
+            st.markdown(message["message"])
+
     # React to user input
     if prompt := st.chat_input("What is up?"):
         # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
+        st.chat_message("USER").markdown(prompt)
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "USER", "message": prompt})
+        # print(st.session_state.messages)
 
-        llm_response = cohereChat(prompt)
-
-        response = f"{llm_response.text}"
-        # Display assistant response in chat message container
+        llm_reponse = cohereReply(prompt)
         with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            st.markdown(llm_reponse)
+        st.session_state.messages.append(
+            {"role": "assistant", "message": llm_reponse})
 
-    
 
-if __name__ == "__main__" :
+
+
+if __name__ == "__main__":
     main()
